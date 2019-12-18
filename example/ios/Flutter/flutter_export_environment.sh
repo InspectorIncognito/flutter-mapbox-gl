397f2d2c2b906d95a1e7fbe14477030c6e0a8e6d
Agustin Antoine <agustin@transapp.cl>
2019-12-18 15:28:55 -0300

    merge with windows code, gitignore needs changes (real one..)

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
+# This is a generated file; do not edit or check into version control.gi
 export "FLUTTER_ROOT=/Users/calvarez/Documents/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/calvarez/Git/flutter-mapbox-gl/example"
 export "FLUTTER_TARGET=lib/main.dart"
@@ -9,11 +8,3 @@ export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/calvarez/Documents/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-=======
-export "FLUTTER_ROOT=C:\Users\Agustin\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Agustin\Desktop\Proyectos\flutter-mapbox-gl\example"
-export "FLUTTER_TARGET=lib\main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\Agustin\Documents\flutter\bin\cache\artifacts\engine\ios"
->>>>>>> 672cf2ffa67a0ee6b667878672fcae28e1ebc1e8
